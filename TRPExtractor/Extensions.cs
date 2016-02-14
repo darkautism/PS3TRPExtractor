98f70f25d33d9edfbd192c50eab224d90ac39298
農至剛 <darkautism@gmail.com>
2016-02-14 19:06:40 +0800

    First published

---
+++ TRPExtractor/Extensions.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
+public static class Extensions {
+    public static byte[] decryptCBC128(this byte[] orginaldata, byte[] key, byte[] iv) {
+        RijndaelManaged AES = new RijndaelManaged();
+        AES.Mode = CipherMode.CBC;
+        AES.Padding = PaddingMode.None;
+        AES.KeySize = 128;
+        AES.Key = key.SubArray(0, 16);
+        AES.IV = iv.SubArray(0, 16);
+        ICryptoTransform transform = AES.CreateDecryptor();
+        byte[] outputData = transform.TransformFinalBlock(orginaldata, 0, orginaldata.Length);
+        return outputData;
+    }
+
+    public static byte[] encryptCBC128(this byte[] orginaldata, byte[] key, byte[] iv) {
+        RijndaelManaged AES = new RijndaelManaged();
+        AES.Mode = CipherMode.CBC;
+        AES.Padding = PaddingMode.None;
+        AES.KeySize = 128;
+        AES.Key = key.SubArray(0, 16);
+        AES.IV = iv.SubArray(0, 16);
+        ICryptoTransform transform = AES.CreateEncryptor();
+        byte[] outputData = transform.TransformFinalBlock(orginaldata, 0, orginaldata.Length);
+        return outputData;
+    }
+
+    public static T[] SubArray<T>(this T[] data, int index, int length) {
+        T[] result = new T[length];
+        Array.Copy(data, index, result, 0, length);
+        return result;
+    }
+
+    public static string ToHexString(this byte[] data) {
+        return BitConverter.ToString(data).Replace("-", "");
+    }
+
+    public static byte[] ToByteArray(this string hex) {
+        return Enumerable.Range(0, hex.Length)
+                         .Where(x => x % 2 == 0)
+                         .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                         .ToArray();
+    }
+
+    public static string ArrayToString<T>(this T[] array) {
+        StringBuilder builder = new StringBuilder("[");
+        for (int i = 0; i < array.GetLength(0); i++) {
+            if (i != 0) builder.Append(",");
+            builder.Append(array[i]);
+        }
+        builder.Append("]");
+        return builder.ToString();
+    }
+
+    public static T ToStruct<T>(this byte[] ptr) {
+        GCHandle handle = GCHandle.Alloc(ptr, GCHandleType.Pinned);
+        T ret = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+        handle.Free();
+        return ret;
+    }
+
+    public static byte[] StructToBytes<T>(this T obj) {
+        byte[] buffer =  new byte[Marshal.SizeOf(typeof(T))];
+        GCHandle handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+        Marshal.StructureToPtr(obj, handle.AddrOfPinnedObject(), false);
+        handle.Free();
+        return buffer;
+    }
+
+    public static short ChangeEndian(this short val) {
+        byte[] arr = BitConverter.GetBytes(val);
+        Array.Reverse(arr);
+        return BitConverter.ToInt16(arr, 0);
+    }
+
+    public static int ChangeEndian(this int val) {
+        byte[] arr = BitConverter.GetBytes(val);
+        Array.Reverse(arr);
+        return BitConverter.ToInt32(arr, 0);
+    }
+
+    public static uint ChangeEndian(this uint val) {
+        byte[] arr = BitConverter.GetBytes(val);
+        Array.Reverse(arr);
+        return BitConverter.ToUInt32(arr, 0);
+    }
+
+    public static long ChangeEndian(this long val) {
+        byte[] arr = BitConverter.GetBytes(val);
+        Array.Reverse(arr);
+        return BitConverter.ToInt64(arr, 0);
+    }
+
+    public static ulong ChangeEndian(this ulong val) {
+        byte[] arr = BitConverter.GetBytes(val);
+        Array.Reverse(arr);
+        return BitConverter.ToUInt64(arr, 0);
+    }
+}
---
+++ TRPExtractor/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace TRPExtractor {
+    static class Program {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main( string[] args ) {
+            if (args.Length != 1) {
+                PrintHelp();
+                return;
+            }
+
+            BigEndianBinaryReader bebr = null;
+            try {
+                bebr = new BigEndianBinaryReader( new FileStream(args[0], FileMode.Open) );
+            } catch (Exception ex) {
+                Console.WriteLine("Error : {0}", ex.Message);
+                return;
+            }
+            Header header = bebr.ReadBytes(Marshal.SizeOf(typeof(Header))).ToStruct<Header>();
+
+            if (header.magic != 0x01000000004DA2DC) {
+                Console.WriteLine("Not a valid TRP file.");
+                return;
+            }
+            FileOffsetInfo[] fileOffsetInfoArray = new FileOffsetInfo[header.AllFileCount];
+            for (int i = 0; i < header.AllFileCount; i++) {
+                fileOffsetInfoArray[i] = bebr.ReadBytes(Marshal.SizeOf(typeof(FileOffsetInfo))).ToStruct<FileOffsetInfo>();
+            }
+
+            foreach (FileOffsetInfo foi in fileOffsetInfoArray) {
+                bebr.BaseStream.Position = foi.Offset;
+                FileStream fs = new FileStream(foi.fileName, FileMode.Create);
+                byte [] towritefiledata = bebr.ReadBytes( (int) foi.FileSize );
+                fs.Write(towritefiledata, 0, towritefiledata.Length);
+                fs.Flush();
+                fs.Close();
+                Console.WriteLine(foi.fileName);
+            }
+
+
+            Console.WriteLine("Extract finished");
+            bebr.Close();
+            return;
+        }
+        public static void PrintHelp() {
+            Console.WriteLine("usage: trpextractor trpfile");
+        }
+
+        #region Structs
+        [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
+        public struct Header {
+
+            /// int
+            public ulong magic;
+
+            /// int
+            public ulong _fileSize;
+
+            /// int
+            private int u1;
+            public int AllFileCount {
+                get {
+                    return u1.ChangeEndian();
+                }
+            }
+
+            /// int
+            public int u2;
+
+            /// byte[40]
+            [System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.ByValArray, SizeConst = 40, ArraySubType = System.Runtime.InteropServices.UnmanagedType.I1)]
+            public byte[] padding;
+        }
+
+        [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential, CharSet = System.Runtime.InteropServices.CharSet.Ansi)]
+        public struct FileOffsetInfo {
+
+            /// char[32]
+            [System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string fileName;
+
+            /// int
+            private long _offset;
+            public long Offset {
+                get {
+                    return _offset.ChangeEndian();
+                }
+            }
+
+            /// int
+            private long _fileSize;
+            public long FileSize {
+                get {
+                    return _fileSize.ChangeEndian();
+                }
+            }
+
+            /// int
+            public int u1;
+
+            /// byte[12]
+            [System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.ByValArray, SizeConst = 12, ArraySubType = System.Runtime.InteropServices.UnmanagedType.I1)]
+            public byte[] padding;
+        }
+
+        #endregion
+    }
+}
